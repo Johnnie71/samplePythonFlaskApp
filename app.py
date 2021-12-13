5246be19ccba2cdce473db5f2252560e5b210b55
johnnie71 <johnnie.agonz71@gmail.com>
2021-12-13 17:36:11 -0500

    using render template to render an html template

--- app.py
+++ app.py
@@ -4,11 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template('base.html')
 
-@app.route('/about')
-def about():
-    return "About Page"
 
 if __name__ == "__main__":
     app.run(debug=True)
